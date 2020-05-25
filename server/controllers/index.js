5b68399b660b31a7c1f1f187ca4f732fb5f28d4f
jeremyrrose <jeremy.r.rose@gmail.com>
2020-05-25 16:46:50 -0400

    /portfolio endpoint, viewPortfolio() controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -28,12 +28,12 @@ const login = async (req, res) => {
 				id: user.id,
 				email: user.email
 			}
-			return res.status(201).json({ payload })
+			return res.status(201).json({ payload });
 		} else {
-			res.status(401).json({ error: 'Username and password do not match.'})
+			res.status(401).json({ error: 'Username and password do not match.'});
 		}
 	} catch (error) {
-		return res.status(500).json({ error: error.message })
+		return res.status(500).json({ error: error.message });
 	}
 }
 
@@ -51,7 +51,7 @@ const newTransaction = async (req, res) => {
             res.status(500).json({ error: 'Transaction could not be created.'})
         }
     } catch (error) {
-        return res.status(500).json({ error: error.message })
+        return res.status(500).json({ error: error.message });
     }
 }
 
@@ -61,7 +61,16 @@ const viewTransactions = async (req, res) => {
         const transactions = await Transaction.find({ _id: user.transactions }, { symbol: 1, price: 1, shares: 1, type: 1, createdAt: 1, _id: 0 })
         res.status(200).json(transactions);
     } catch (error) {
-        return res.status(500).json({ error: error.message })
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+const viewPortfolio = async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.body.user });
+        res.status(200).json({ cashBalance: user.cashBalance, portfolio: user.portfolio });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
     }
 }
 
@@ -69,5 +78,6 @@ module.exports = {
     register,
     login,
     newTransaction,
-    viewTransactions
+    viewTransactions,
+    viewPortfolio
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ router.get('/', (req, res) => res.send('This is rooot!'))
 
 router.post('/register', controllers.register);
 router.post('/login', controllers.login);
+router.post('/portfolio', controllers.viewPortfolio);
 router.post('/transactions/new', controllers.newTransaction);
 router.post('/transactions', controllers.viewTransactions);
 
